01f0f2128ec69aabb88926bd48ff8c1a2423fbc6
Max Scheiber <max@chegg.com>
2013-07-27 16:11:08 -0700

    fix typo on 1

--- 1.py
+++ 1.py
@@ -4,4 +4,4 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-print sum([n for n in range(3, 1000) if  n % 3 == 0 or n % 5 == 0])
+print sum([n for n in range(3, 1000) if n % 3 == 0 or n % 5 == 0])
